0d48232be5494b87190fdbff14337d0b054b734c
Seshan Ravikumar <seshan10@me.com>
2021-09-18 02:35:23 -0400

    changes

--- proxy/index.js
+++ proxy/index.js
@@ -18,6 +18,6 @@ proxy.on('cycle', function (badHost, newHost) {
   // `cycle` event happens when proxy couldn't contact the backend host and
   // switched to the next server. You can put your own reporting logic in here.
   //
-  console.log("Switched from " + badHost + " to " + newHost);
+  console.log("Switched from " + JSON.stringify(badHost) + " to " + JSON.stringify(newHost));
 });
 console.log("Proxy is listening");
