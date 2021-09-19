f1a80e05b4e7af1396b29d058016ae1c9d161092
Seshan Ravikumar <seshan10@me.com>
2021-09-19 00:35:40 -0400

    SQL Source

--- backend/create_tables.sql
+++ backend/create_tables.sql
@@ -1,9 +1,25 @@
-create table state_changes
+create table users
 (
-	id serial
-		constraint state_changes_pk
+	id bigint default unique_rowid() not null
+		constraint "primary"
 			primary key,
 	username varchar,
-	"when" timestamp,
+	password varchar,
+	displayname varchar,
 	action jsonb
 );
+
+alter table users owner to root;
+
+create table state_changes
+(
+    id        bigint default unique_rowid() not null
+        constraint state_changes_pk
+            primary key,
+    username  varchar,
+    timestamp timestamp,
+    action    jsonb
+);
+
+alter table state_changes
+    owner to root;
